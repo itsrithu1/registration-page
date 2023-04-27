db442a125539e591d7e56ffe0912d474dd073dbb
aayushanand <aayushanand04@gmail.com>
2023-04-27 23:52:24 +0530

    routes added along with backend integration

--- src/App.js
+++ src/App.js
@@ -4,40 +4,19 @@ import './App.css';
 import Register from './Register';
 import Login from './SignIn';
 import HomePage from './Homepage';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <main className="App">
-     {/* <Routes>
-        <Route path='/' element={<Register/>} />
-        <Route path='/SignIn' element={<Login/>} />
-        {/* <Route path='/HomePage' element={<HomePage/>} /> */}
-      {/*</Routes> */}
-{/* 
       <BrowserRouter>
         <Routes>
-          <Route path='/' component={<Register/>} />
-          <Route path='/SignIn' component={<Login/>} />
-          <Route path='/HomePage' component={<HomePage/>} />
+          <Route path='/' element={<Navigate to="/SignIn" replace={true} />} />
+          <Route path='/Register' element={<Register />} />
+          <Route path='/SignIn' element={<Login />} Default/>
+          <Route path='/HomePage' element={<HomePage />} />
         </Routes>
-      </BrowserRouter> */}
-{/* 
-      <React.Fragment>
-
-        <Router>
-          <Routes>
-            <Route path='/' element={<HomePage />}/>
-          </Routes>
-        </Router>
-
-      </React.Fragment>
-   <Register/> */}
-   <Login/>
-   <Routes>
-    <Route path='/Register' element={<Register />}/>
-
-   </Routes>
+      </BrowserRouter>
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-   
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
